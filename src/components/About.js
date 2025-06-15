25cc38e72ff42335ee124fa0a28d2f7160139a1e
PabloSczn <sancheznarro.pablo@gmail.com>
2025-06-15 18:19:50 +0100

    improve landing and add About section

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section id="about" style={{
+      minHeight: '100vh',
+      padding: '4rem 2rem',
+      background: '#111',
+      color: '#fff',
+    }}>
+      <h2>About The Adel Gomez Band</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec
+        vestibulum magna, et dapibus lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      {/* …more content… */}
+    </section>
+  );
+}
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaEnvelope, FaInstagram, FaFacebookF, FaXTwitter, FaSpotify, FaApple, FaYoutube } from 'react-icons/fa6';
+import { FaChevronDown } from 'react-icons/fa';
 import bg from '../assets/hero-background.jpg';
 
 export default function Hero() {
@@ -12,18 +13,33 @@ export default function Hero() {
         position: 'relative'
       }}
     >
-      <div className="hero-content">
-        <button>DISCOVER US!</button>
-        <div className="social-icons">
-          <a aria-label="Join mailing list"  href="/#email"><FaEnvelope/></a>
-          <a aria-label="Instagram" target="_blank" rel="noreferrer" href="https://instagram.com/"><FaInstagram/></a>
-          <a aria-label="Facebook"  target="_blank" rel="noreferrer" href="https://facebook.com/"><FaFacebookF/></a>
-          <a aria-label="X / Twitter" target="_blank" rel="noreferrer" href="https://twitter.com/"><FaXTwitter/></a>
-          <a aria-label="Spotify"   target="_blank" rel="noreferrer" href="https://open.spotify.com/artist/"><FaSpotify/></a>
-          <a aria-label="Apple Music" target="_blank" rel="noreferrer" href="https://music.apple.com/us/artist/"><FaApple/></a>
-          <a aria-label="YouTube"  target="_blank" rel="noreferrer" href="https://youtube.com/user/"><FaYoutube/></a>
+        <div className="hero-socials">
+            <a aria-label="Join mailing list"  href="/#email"><FaEnvelope/></a>
+            <a aria-label="Instagram" target="_blank" rel="noreferrer" href="https://instagram.com/"><FaInstagram/></a>
+            <a aria-label="Facebook"  target="_blank" rel="noreferrer" href="https://facebook.com/"><FaFacebookF/></a>
+            <a aria-label="X / Twitter" target="_blank" rel="noreferrer" href="https://twitter.com/"><FaXTwitter/></a>
+            <a aria-label="Spotify"   target="_blank" rel="noreferrer" href="https://open.spotify.com/artist/"><FaSpotify/></a>
+            <a aria-label="Apple Music" target="_blank" rel="noreferrer" href="https://music.apple.com/us/artist/"><FaApple/></a>
+            <a aria-label="YouTube"  target="_blank" rel="noreferrer" href="https://youtube.com/user/"><FaYoutube/></a>
         </div>
-      </div>
+        <div className="hero-content">
+            <button>DISCOVER US!</button>
+        </div>
+        <a
+            href="#about"
+            aria-label="Scroll down"
+            style={{
+            position: 'absolute',
+            bottom: '2rem',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            fontSize: '2rem',
+            color: 'rgba(255,255,255,0.8)',
+            animation: 'bounce 2s infinite'
+            }}
+        >
+            <FaChevronDown />
+        </a>
     </section>
   );
 }
