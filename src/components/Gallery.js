84b0d0153f48e28e1d167b3e0eb8d8d55137e52d
PabloSczn <sancheznarro.pablo@gmail.com>
2025-06-21 20:08:23 +0100

    add photos to gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,8 +5,14 @@ import gallery1 from '../assets/gallery1.jpg';
 import gallery2 from '../assets/gallery2.jpg';
 import gallery3 from '../assets/gallery3.jpg';
 import gallery4 from '../assets/gallery4.jpg';
+import gallery5 from '../assets/gallery5.jpg';
+import gallery6 from '../assets/gallery6.jpg';
+import gallery7 from '../assets/gallery7.jpg';
+import gallery8 from '../assets/gallery8.jpg';
+import gallery9 from '../assets/gallery9.jpg';
+import gallery10 from '../assets/gallery10.jpg';
 
-const images = [gallery1, gallery2, gallery3, gallery4];
+const images = [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9, gallery10];
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
